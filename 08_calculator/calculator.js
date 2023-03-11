0460f806489621416053558de319cae9862dd24e
askarbekov-d-auca-2022 <askarbekov_d@auca.kg>
2023-03-11 11:39:53 +0600

    Make initial changes

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(total, numbers) {
+	for (let i = 0; i > 0; i++)
+  numbers += total
+
+  // Think about this one
 };
 
-const multiply = function() {
-
+const multiply = function(a, b) {
+  return a * b;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+let answer = 1;
+if (a === 0 || a === 1) {
+  return answer;
+} else if (a > 1) {
+  for (let i = a; i >= 1; i--){
+    answer = answer * i
+  }
+  } else {
+    return "The number has to be positive";
+  }
 };
 
 // Do not edit below this line
