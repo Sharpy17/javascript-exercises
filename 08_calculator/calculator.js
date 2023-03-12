86159a998ca5e869a94b7a602921633112c68496
askarbekov-d-auca-2022 <askarbekov_d@auca.kg>
2023-03-12 12:29:49 +0600

    Complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,15 +6,20 @@ const subtract = function(a, b) {
 	return a - b;
 };
 
-const sum = function(total, numbers) {
-	for (let i = 0; i > 0; i++)
-  numbers += total
-
-  // Think about this one
+const sum = function(arr) {
+  let total = 0;
+	for (let i = 0; i < arr.length; i++) {
+  total += arr[i];
+  };
+  return total;
 };
 
-const multiply = function(a, b) {
-  return a * b;
+const multiply = function(arr) {
+  let total = 1;
+  for (let i = 0; i < arr.length; i++) {
+    total *= arr[i]
+  }
+  return total;
 };
 
 const power = function(a, b) {
@@ -27,8 +32,8 @@ if (a === 0 || a === 1) {
   return answer;
 } else if (a > 1) {
   for (let i = a; i >= 1; i--){
-    answer = answer * i
-  }
+    answer = answer * i;
+  } return answer;
   } else {
     return "The number has to be positive";
   }
