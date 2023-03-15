a77f22073db338b15d2ff5317a75e361381b71cd
askarbekov-d-auca-2022 <askarbekov_d@auca.kg>
2023-03-15 12:47:59 +0600

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-const findTheOldest = function() {
-
+const findTheOldest = (array) => {
+    const currentYear = (new Date()).getFullYear();
+    const oldest = array
+                    .sort((aGuy, bGuy) => {
+                        if (!aGuy.yearOfDeath && bGuy.yearOfDeath) {
+                            const firstLife = currentYear - aGuy.yearOfBirth;
+                            const secondLife = bGuy.yearOfDeath - bGuy.yearOfBirth;
+                            return (firstLife > secondLife ? -1 : 1);
+                        } else if (aGuy.yearOfDeath && !bGuy.yearOfDeath) {
+                        const firstLife = aGuy.yearOfDeath - aGuy.yearOfBirth;
+                        const secondLife = currentYear - bGuy.yearOfBirth;
+                        return (firstLife > secondLife ? -1 : 1);
+                        } else if (!aGuy.yearOfDeath && !bGuy.yearOfBirth) {
+                            const firstLife = currentYear - aGuy.yearOfBirth;
+                            const secondLife = currentYear - bGuy.yearOfBirth;
+                            return (firstLife > secondLife ? -1 : 1);
+                        } else {
+                            const firstLife = aGuy.yearOfDeath - aGuy.yearOfBirth;
+                            const secondLife = bGuy.yearOfDeath - bGuy.yearOfBirth;
+                            return (firstLife > secondLife ? -1 : 1);
+                        }
+                    })
+                    .find((guy) => (guy.name));
+    return oldest;
 };
 
 // Do not edit below this line
