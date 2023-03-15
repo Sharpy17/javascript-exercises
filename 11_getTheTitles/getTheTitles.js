8dc81782988cf9f36aa80f745c485ba997e1114f
askarbekov-d-auca-2022 <askarbekov_d@auca.kg>
2023-03-15 12:01:13 +0600

    Complete exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = (obj) => {
+    const titles = obj.map((book) => {
+        return book.title;
+    });
+    return titles;
 };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
